17448fa30d542958b90887be6368ffdca6dcea13
JoaquinCorpuz <130562568+joaquincorpuz21@users.noreply.github.com>
2023-05-07 22:09:51 +0800

    Add files via upload

---
+++ petname.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pets (
+  name VARCHAR(30),
+  gender CHAR(6),
+  birthdate DATE,
+  owner VARCHAR(70),
+  email VARCHAR(100),
+  address VARCHAR(255),
+  contact_number VARCHAR(20)
+);
